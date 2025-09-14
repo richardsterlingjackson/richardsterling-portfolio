c097261505e0a30bd61120b5f78659921d0e54e0
RichSterling <Richard.Sterling.Jackson@gmail.com>
2025-09-13 22:50:57 -0700

    Initial commit: Angular counter component for portfolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CounterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('counter');
+  
 }
